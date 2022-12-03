5772720b8112ca738e549da456682bb4f4805dae
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-03 22:06:16 +0200

    Add the ipad container image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Fragment } from 'react';
 import classes from './App.module.scss';
 import { Quiz } from './components';
+import ipad from './assets/ipad.svg';
 
 function App() {
   return (
     <div className={classes.container}>
-      <div className={classes.quiz}>
-        <Quiz/>
+      <div className={classes.imageContainer}>
+        <img className={classes.image} src={ipad} alt="iPad device"/>
+
+        <div className={classes.quiz}>
+          <Quiz/>
+        </div>
       </div>
     </div>
   );
