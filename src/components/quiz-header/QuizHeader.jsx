9e86ee3f06405fd8df350d1a8af548796db1e7d9
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-03 21:07:45 +0200

    Change the quiz header handler

--- src/components/quiz-header/QuizHeader.jsx
+++ src/components/quiz-header/QuizHeader.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Timer from '../timer/Timer';
 import classes from './QuizHeader.module.scss';
 
 const QuizHeader = ({ currentQuestion, allQuestions, timeoutCallback }) => {
+  const [currentNumber, setCurrentNumber] = useState();
+
+  useEffect(() => setCurrentNumber(currentQuestion), [currentQuestion]);
+
   return (
     <div>
-      <Timer seconds={10} timeoutCallback={timeoutCallback}/>
+      <Timer seconds={10} timeoutCallback={timeoutCallback} shouldRerender={{}}/>
 
       <h1 className={classes.counter}>
-        <span className={classes.numberOfQuestion}>{currentQuestion}</span>
+        <span className={classes.numberOfQuestion}>{currentNumber}</span>
         &nbsp;of&nbsp;
         <span className={classes.numberOfQuestion}>{allQuestions}</span>
       </h1>
