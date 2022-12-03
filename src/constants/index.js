3c72f8b988a48da8d3567e2828ee9247520c1e1c
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-03 16:02:15 +0200

    Add the bar colors constant

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import { PROGRESS_BAR_COLORS } from './progress-bar-colors';
+
+export {
+  PROGRESS_BAR_COLORS,
+};
---
+++ src/constants/progress-bar-colors.js
@@ -0,0 +1,5 @@
+export const PROGRESS_BAR_COLORS = Object.freeze({
+  green: 'progress-good',
+  orange: 'progress-alert',
+  red: 'progress-danger',
+});
