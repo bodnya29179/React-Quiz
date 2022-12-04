7635676c062fd79f8bb97d1bcdc18f7960591974
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-04 13:01:37 +0200

    Add the start button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { Fragment } from 'react';
 import classes from './App.module.scss';
 import { Quiz } from './components';
 import ipad from './assets/ipad.svg';
+import button from './assets/button.svg';
+import { useState } from 'react';
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const startTheGame = () => setIsGameStarted(true);
+
   return (
     <div className={classes.container}>
       <div className={classes.imageContainer}>
-        <img className={classes.image} src={ipad} alt="iPad device"/>
+        <img className={classes.device} src={ipad} alt="iPad device"/>
+
+        <button className={classes.startButton} onClick={startTheGame}>
+          <img className={classes.buttonIcon} src={button} alt="Start button"/>
+        </button>
 
         <div className={classes.quiz}>
-          <Quiz/>
+          { isGameStarted && <Quiz/> }
         </div>
       </div>
     </div>
